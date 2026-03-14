b0d914076a83a0c3e68e8b0344d774a3fd9ce496
NikS <59556607+niksdev09@users.noreply.github.com>
2026-03-14 06:50:26 +0000

    feat: add todo, table and button with handling

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,4 +17,28 @@ export class HomePage {
 
   constructor() {}
   myName: string = "Никита"
+  todo: string[] = [
+    "Купить молоко",
+    "Пойти в магазин",
+    "Сделать домашку",
+    "Пойти на тренировку",
+    "Пойти спать"
+  ]
+  books: Book[] = [
+    {
+      id: 1,
+      title: "Книга 1",
+      author: "Пушкин А.С.",
+      description: "Описание книги 1"
+    },
+    {
+      id: 2,
+      title: "Книга 2",
+      author: "Толстой Л.Н.",
+      description: "Описание книги 2"
+    }
+  ];
+  btnClick: () => void = () => {
+    this.myName = this.myName + "!";
+  }
 }
